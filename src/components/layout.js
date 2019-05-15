b7a51ebcc12fdc78337a4082acd4468fcb97e65a
Victor Kane <victorkane@gmail.com>
2019-05-15 05:26:43 -0400

    Initial material-ui installation

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,25 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
+import Helmet from 'react-helmet'
 import Header from "./header"
-import "./layout.css"
+import Navigation from './Navigation/Navigation';
+
+import { withStyles } from '@material-ui/core/styles';
+import withRoot from '../withRoot';
+
+const styles = theme => ({
+    root: {
+          width: 'auto',
+          marginLeft: theme.spacing.unit * 3,
+          marginRight: theme.spacing.unit * 3,
+          [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+                  width: 1100,
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                },
+        },
+});
 
 const Layout = ({ children }) => (
   <StaticQuery
